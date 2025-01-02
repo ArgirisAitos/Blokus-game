c0350c700fb14497523e085c09b451730432afe1
Argiris Aitos <aitos.ar49@gmail.com>
2025-01-02 16:59:30 +0200

    Update Blokus.php

--- Βlokus.php
+++ Βlokus.php
@@ -38,39 +38,41 @@ switch ($r = array_shift($request)) {
         }
         break;
 
-        case 'pass':
-            if ($method == 'POST') {
-                $game_id = array_shift($request);
-                passTurn($game_id,$input);
-            } else {
-                header("HTTP/1.1 405 Method Not Allowed");
-            }
-            break;
+    case 'pass':
+        if ($method == 'POST') {
+            $game_id = array_shift($request);
+            passTurn($game_id,$input);
+        } else {
+            header("HTTP/1.1 405 Method Not Allowed");
+        }
+        break;
+
+    case 'board':
+        if ($method == 'GET') {
+            $game_id = array_shift($request);
+            viewBoard($game_id);
+        } else {
+            header("HTTP/1.1 405 Method Not Allowed");
+        }
+        break;
+
+    case 'shapes':
+        if ($method == 'GET') {
+            $game_id = array_shift($request);
+            AvailableShapes($game_id);
+        } else {
+            header("HTTP/1.1 405 Method Not Allowed");
+        }
+        break;
 
 
-            case 'board':
-                if ($method == 'GET') {
-                    $game_id = array_shift($request);
-                    viewBoard($game_id);
-                } else {
-                    header("HTTP/1.1 405 Method Not Allowed");
-                }
-                break;
 
 
-                case 'shapes':
-                    if ($method == 'GET') {
-                        $game_id = array_shift($request);
-                        AvailableShapes($game_id);
-                    } else {
-                        header("HTTP/1.1 405 Method Not Allowed");
-                    }
-                    break;
+        
 
     default:
         header("HTTP/1.1 404 Not Found");
         echo json_encode(['error' => "Endpoint '$r' not found."]);
         break;
 }
-
 ?>
